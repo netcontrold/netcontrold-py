b87b6df5f815d8cc38fb9a4aa3309f9bd8bde11d
veda <veda.b16@iiits.in>
2020-02-27 13:13:39 +0530

    changed test_util file modified cpuinfo function in util.py
    unittestcases for cpuinfo function

--- netcontrold/tests/lib/test_util.py
+++ netcontrold/tests/lib/test_util.py
@@ -19,17 +19,96 @@ from unittest import TestCase
 from netcontrold.lib import util
 
 _BASIC_CPU_INFO = """
-processor	: 0
+processor       : 1
+core id         : 0
+physical id     : 0
+
+processor       : 1
+core id         : 1
+physical id     : 
+
+processor       : 2
+core id         : 0
+physical id     : 
+
+processor       : 3
+core id         : 1
+physical id     : 0
 """
 
+_BASIC_CPU_INFO_0 = """
+core id         : 0
+
+processor		: 1
+core id         : 0
+physical id     : 
+
+processor		: 1
+core id 		: 
+physical id     : 0
+
+processor		: 2
+core id         : 0
+physical id 	: 0
+
+processor		: 
+core id         : 1
+physical id     : 0
+"""
+
+_BASIC_CPU_INFO_1 = """
+processor		: 0
+core id 		: 0
+physical id 	: 0
+
+processor		: 1
+core id 		: 1
+physical id 	: 0
+
+processor		: 2
+core id 		: 0
+physical id 	: 0
+
+processor		: 3
+core id 		: 1
+physical id 	: 0
+"""
 
 class TestUtil(TestCase):
 
     @mock.patch('netcontrold.lib.util.open')
     def test_cpuinfo_basic(self, mock_open):
         mock_open.side_effect = [
-            mock.mock_open(read_data=_BASIC_CPU_INFO).return_value
+            mock.mock_open(read_data=_BASIC_CPU_INFO_0).return_value
+        ]
+        self.assertRaises(ValueError,util.cpuinfo)
+    
+    @mock.patch('netcontrold.lib.util.open')
+    def test_cpuinfo_basic0(self, mock_open):
+        mock_open.side_effect = [
+            mock.mock_open(read_data=_BASIC_CPU_INFO_1).return_value
+        ]
+        expected = [{'processor':'0','core id':'0','physical id':'0'},{'processor':'1','core id':'1','physical id':'0'},{'processor':'2','core id':'0','physical id':'0'},{'processor':'3','core id':'1','physical id':'0'}]
+        out = util.cpuinfo()
+        self.assertEqual(out, expected)
+
+    @mock.patch('netcontrold.lib.util.open')
+    def test_cpuinfo_empty(self, mock_open):
+        mock_open.side_effect = [
+            mock.mock_open(read_data="").return_value
         ]
         out = util.cpuinfo()
-        expected = [{'processor': '0'}]
+        expected = []
         self.assertEqual(out, expected)
+
+    @mock.patch('netcontrold.lib.util.open')
+    def test_numa_cpu_map(self, mock_open):
+        mock_open.side_effect = [
+            mock.mock_open(read_data=_BASIC_CPU_INFO_1).return_value
+        ]
+        out = util.numa_cpu_map()
+        expected = {0:{0:[0,2],1:[1,3]}}
+        self.assertEqual(out, expected)
+
+    
+
