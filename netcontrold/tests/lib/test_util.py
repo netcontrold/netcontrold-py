2c81591777d7e779365d080b5eee4b153a04d2ae
Gowrishankar Muthukrishnan <gmuthukr@redhat.com>
2020-02-29 20:52:48 +0530

    PEP8 corrections applied on test_util.py
    Signed-off-by: Gowrishankar Muthukrishnan <gmuthukr@redhat.com

--- netcontrold/tests/lib/test_util.py
+++ netcontrold/tests/lib/test_util.py
@@ -47,6 +47,7 @@ core id 		: 1
 physical id 	: 0
 """
 
+
 class TestUtil(TestCase):
 
     @mock.patch('netcontrold.lib.util.open')
@@ -54,15 +55,17 @@ class TestUtil(TestCase):
         mock_open.side_effect = [
             mock.mock_open(read_data=_BASIC_CPU_INFO_0).return_value
         ]
-        expected = [{'processor':'0','core id':'0'},{'processor': '1','physical id':'','core id':'xyz'},{'processor': '2'}]
-        self.assertRaises(ValueError,util.cpuinfo)
-    
+        expected = [{'processor': '0', 'core id': '0'}, {
+            'processor': '1', 'physical id': '', 'core id': 'xyz'}, {'processor': '2'}]
+        self.assertRaises(ValueError, util.cpuinfo)
+
     @mock.patch('netcontrold.lib.util.open')
     def test_cpuinfo_basic0(self, mock_open):
         mock_open.side_effect = [
             mock.mock_open(read_data=_BASIC_CPU_INFO_1).return_value
         ]
-        expected = [{'processor':'0','core id':'0','physical id':'0'},{'processor':'1','core id':'1','physical id':'0'},{'processor':'2','core id':'0','physical id':'0'},{'processor':'3','core id':'1','physical id':'0'}]
+        expected = [{'processor': '0', 'core id': '0', 'physical id': '0'}, {'processor': '1', 'core id': '1', 'physical id': '0'}, {
+            'processor': '2', 'core id': '0', 'physical id': '0'}, {'processor': '3', 'core id': '1', 'physical id': '0'}]
         out = util.cpuinfo()
         self.assertEqual(out, expected)
 
@@ -81,6 +84,5 @@ class TestUtil(TestCase):
             mock.mock_open(read_data=_BASIC_CPU_INFO_1).return_value
         ]
         out = util.numa_cpu_map()
-        expected = {0:{0:[0,2],1:[1,3]}}
+        expected = {0: {0: [0, 2], 1: [1, 3]}}
         self.assertEqual(out, expected)
-
