6682b4897103a14011c54ce9d6c97161f191ccfc
Shrutikabansal <ssb2361998@gmail.com>
2020-03-04 14:11:28 +0530

    test cases updated with class

--- netcontrold/tests/lib/test_util.py
+++ netcontrold/tests/lib/test_util.py
@@ -182,6 +182,37 @@ class TestUtil_cpuinfo(TestCase):
 
 
 
+
+class TestUtil_numa_cpu_map(TestCase):
+
+    @mock.patch('netcontrold.lib.util.open')
+    def test_numa_cpu_map_basic_proclist(self, mock_open):
+        mock_open.side_effect = [
+            mock.mock_open(read_data=_BASIC_CPU_INFO_0_proclist).return_value
+        ]
+        self.assertRaises(ValueError, util.numa_cpu_map)
+
+    @mock.patch('netcontrold.lib.util.open')
+    def test_numa_cpu_map_basic_processor(self, mock_open):
+        mock_open.side_effect = [
+            mock.mock_open(read_data=_BASIC_CPU_INFO_0_processor).return_value
+        ]
+        self.assertRaises(ValueError, util.numa_cpu_map)
+
+    @mock.patch('netcontrold.lib.util.open')
+    def test_numa_cpu_map_basic_coreid(self, mock_open):
+        mock_open.side_effect = [
+            mock.mock_open(read_data=_BASIC_CPU_INFO_0_coreid).return_value
+        ]
+        self.assertRaises(ValueError, util.numa_cpu_map)
+
+    @mock.patch('netcontrold.lib.util.open')
+    def test_numa_cpu_map_basic_physicalid(self, mock_open):
+        mock_open.side_effect = [
+            mock.mock_open(read_data=_BASIC_CPU_INFO_0_physicalid).return_value
+        ]
+        self.assertRaises(ValueError, util.numa_cpu_map)
+
     @mock.patch('netcontrold.lib.util.open')
     def test_numa_cpu_map_positive(self, mock_open):
         mock_open.side_effect = [
@@ -191,13 +222,39 @@ class TestUtil_cpuinfo(TestCase):
         expected = {0: {0: [0, 2], 1: [1, 3]}}
         self.assertEqual(out, expected)
 
+
+
+
+class TestUtil_rr_cpu_in_numa(TestCase):
+
     @mock.patch('netcontrold.lib.util.open')
-    def test_numa_cpu_map_negative(self, mock_open):
+    def test_rr_cpu_in_numa_basic_proclist(self, mock_open):
+        mock_open.side_effect = [
+            mock.mock_open(read_data=_BASIC_CPU_INFO_0_proclist).return_value
+        ]
+        self.assertRaises(ValueError, util.rr_cpu_in_numa)
+
+    @mock.patch('netcontrold.lib.util.open')
+    def test_rr_cpu_in_numa_basic_processor(self, mock_open):
+        mock_open.side_effect = [
+            mock.mock_open(read_data=_BASIC_CPU_INFO_0_processor).return_value
+        ]
+        self.assertRaises(ValueError, util.rr_cpu_in_numa)
+
+    @mock.patch('netcontrold.lib.util.open')
+    def test_rr_cpu_in_numa_basic_coreid(self, mock_open):
+        mock_open.side_effect = [
+            mock.mock_open(read_data=_BASIC_CPU_INFO_0_coreid).return_value
+        ]
+        self.assertRaises(ValueError, util.rr_cpu_in_numa)
+
+    @mock.patch('netcontrold.lib.util.open')
+    def test_rr_cpu_in_numa_basic_physicalid(self, mock_open):
         mock_open.side_effect = [
             mock.mock_open(read_data=_BASIC_CPU_INFO_0_physicalid).return_value
         ]
-        self.assertRaises(ValueError, util.numa_cpu_map)
-
+        self.assertRaises(ValueError, util.rr_cpu_in_numa)
+        
     @mock.patch('netcontrold.lib.util.open')
     def test_rr_cpu_in_numa_positive(self, mock_open):
         mock_open.side_effect = [
@@ -206,10 +263,3 @@ class TestUtil_cpuinfo(TestCase):
         out = util.rr_cpu_in_numa()
         expected = [0, 2, 1, 3]
         self.assertEqual(out, expected)
-
-    @mock.patch('netcontrold.lib.util.open')
-    def test_rr_cpu_in_numa_negative(self, mock_open):
-        mock_open.side_effect = [
-            mock.mock_open(read_data=_BASIC_CPU_INFO_0_physicalid).return_value
-        ]
-        self.assertRaises(ValueError, util.rr_cpu_in_numa)
