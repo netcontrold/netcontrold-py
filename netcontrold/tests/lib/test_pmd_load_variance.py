680009f5358c4b7a8728873fd4d5cb1292e008ae
Shrutikabansal <ssb2361998@gmail.com>
2020-03-11 12:28:07 +0530

    pmd_load_variance basic test case added

--- netcontrold/tests/lib/test_pmd_load_variance.py
+++ netcontrold/tests/lib/test_pmd_load_variance.py
@@ -13,46 +13,39 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-
 import copy
-from unittest import mock
 from unittest import TestCase
-
-from netcontrold.app import ncd
+from unittest import mock
 from netcontrold.lib import dataif
 from netcontrold.lib import config
 
-_BASIC_DATA = """
-pmd thread numa_id 0 core_id 22:
-    emc hits:17461158
-    megaflow hits:0
-    avg. subtable lookups per hit:0.00
-    miss:0
-    lost:0
-    polling cycles:4948219259 (25.81%)
-    processing cycles:14220835107 (74.19%)
-    avg cycles per packet: 1097.81 (19169054366/17461158)
-    avg processing cycles per packet: 814.43 (14220835107/17461158)
---
-pmd thread numa_id 0 core_id 2:
-    emc hits:14874381
-    megaflow hits:0
-    avg. subtable lookups per hit:0.00
-    miss:0
-    lost:0
-    polling cycles:5460724802 (29.10%)
-    processing cycles:13305794333 (70.90%)
-    avg cycles per packet: 1261.67 (18766519135/14874381)
-    avg processing cycles per packet: 894.54 (13305794333/14874381)
+_FX_CPU_INFO = """
+processor       : 0
+core id         : 0
+physical id     : 0
+
+processor       : 1
+core id         : 1
+physical id     : 0
+
+processor       : 2
+core id         : 0
+physical id     : 0
+
+processor       : 3
+core id         : 1
+physical id     : 0
 """
 
 
-
-class TestUtil_pmd_load_variance(TestCase):
+class TestRebalDryrunRR_OnePmd(TestCase):
+    """
+    Test rebalance for one or more rxq handled by one pmd.
+    """
 
     pmd_map = dict()
     core_id = 0
-    
+
     # setup test environment
     def setUp(self):
         # turn off limited info shown in assert failure for pmd object.
@@ -67,28 +60,45 @@ class TestUtil_pmd_load_variance(TestCase):
         dataif.Context.nlog = NlogNoop()
 
         # create one pmd object.
-        self.pmd = dataif.Dataif_Pmd(self.core_id)
+        fx_pmd = dataif.Dataif_Pmd(self.core_id)
 
         # let it be in numa 0.
-        self.pmd.numa_id = 0
+        fx_pmd.numa_id = 0
 
         # add some cpu consumption for this pmd.
         for i in range(0, config.ncd_samples_max):
-            self.pmd.idle_cpu_cyc[i] = (1000 + (100 * i))
-            self.pmd.proc_cpu_cyc[i] = (5000 + (500 * i))
-            self.pmd.rx_cyc[i] = (10000 + (100 * i))
+            fx_pmd.idle_cpu_cyc[i] = (1000 + (100 * i))
+            fx_pmd.proc_cpu_cyc[i] = (5000 + (500 * i))
+            fx_pmd.rx_cyc[i] = (10000 + (100 * i))
 
-        #print(self.pmd)
-        self.pmd_map[self.core_id] = self.pmd
+        self.pmd_map[self.core_id] = fx_pmd
         return
 
-    @mock.patch('netcontrold.lib.dataif.open')
-    def test_pmd_load_positive(self, mock_open):
-        mock_open.side_effect = [
-            mock.mock_open(read_data=_BASIC_DATA).return_value
-        ]
-        pmd_load=dataif.pmd_load(self.pmd)
-        dataif.update_pmd_load(self.pmd_map)
-        out = dataif.pmd_load_variance(self.pmd_map)
-        expected = 0 
-        self.assertEqual(out, expected)
+   
+    def test_one_rxq(self):
+        # retrieve pmd object.
+        pmd = self.pmd_map[self.core_id]
+
+        # one dummy port is required for this test.
+        port_name = 'virtport'
+
+        # create port class of name 'virtport'.
+        dataif.make_dataif_port(port_name)
+
+        # add port object into pmd.
+        fx_port = pmd.add_port(port_name)
+        fx_port.numa_id = pmd.numa_id
+
+        # add a dummy rxq into port.
+        fx_rxq = fx_port.add_rxq(0)
+
+        # add some cpu consumption for this rxq.
+        for i in range(0, config.ncd_samples_max):
+            fx_rxq.cpu_cyc[i] = (1000 + (100 * i))
+
+        n_reb_rxq = dataif.pmd_load_variance(self.pmd_map)
+
+        self.assertEqual(n_reb_rxq, 0)
+
+        # del port object from pmd.
+        pmd.del_port(port_name)
