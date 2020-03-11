4a2e33b853f673597729ca5eae1e5778d60a583a
veda <veda.b16@iiits.in>
2020-03-11 12:46:14 +0530

    modified preliminary tests

--- netcontrold/tests/lib/test_prelim.py
+++ netcontrold/tests/lib/test_prelim.py
@@ -17,52 +17,49 @@
 from unittest import TestCase
 
 from netcontrold.lib import dataif
-from netcontrold.lib.error import NcdException,ObjConsistencyExc
+from netcontrold.lib.error import NcdException, ObjConsistencyExc
+
 
 class TestDataif_add_rxq(TestCase):
 
-	def setUp(self):
-		self.port1= dataif.Port("port1")
+    def setUp(self):
+        self.port1 = dataif.Port("port1")
 
+    def test_port_add_rxq_1(self):
+        rxq1 = self.port1.add_rxq(1)
+        self.assertEqual(self.port1.find_rxq_by_id(1), rxq1)
 
-	def test_port_add_rxq_1(self):
-		rxq1=self.port1.add_rxq(1)
-		self.assertEqual(self.port1.find_rxq_by_id(1),rxq1)
-
-	def test_port_add_rxq_2(self):
-		rxq2=self.port1.add_rxq(2)
-		with self.assertRaises(ObjConsistencyExc):
-			self.port1.add_rxq(2)
+    def test_port_add_rxq_2(self):
+        rxq2 = self.port1.add_rxq(2)
+        with self.assertRaises(ObjConsistencyExc):
+            self.port1.add_rxq(2)
 
 
 class TestDataif_del_rxq(TestCase):
 
-	def setUp(self):
-		self.port1= dataif.Port("port1")
-		self.rxq1=self.port1.add_rxq(1)
-		self.rxq2=self.port1.add_rxq(2)
+    def setUp(self):
+        self.port1 = dataif.Port("port1")
+        self.rxq1 = self.port1.add_rxq(1)
+        self.rxq2 = self.port1.add_rxq(2)
 
+    def test_port_del_rxq_1(self):
+        self.port1.del_rxq(1)
+        self.assertEqual(self.port1.find_rxq_by_id(1), None)
 
-	def test_port_del_rxq_1(self):
-		self.port1.del_rxq(1)
-		self.assertEqual(self.port1.find_rxq_by_id(1),None)
-
-	def test_port_del_rxq_2(self):
-		with self.assertRaises(ObjConsistencyExc):
-			self.port1.del_rxq(3)
+    def test_port_del_rxq_2(self):
+        with self.assertRaises(ObjConsistencyExc):
+            self.port1.del_rxq(3)
 
 
 class TestDataif_find_rxq(TestCase):
 
-	def setUp(self):
-		self.port1= dataif.Port("port1")
-		self.rxq1=self.port1.add_rxq(1)
-		self.rxq2=self.port1.add_rxq(2)
+    def setUp(self):
+        self.port1 = dataif.Port("port1")
+        self.rxq1 = self.port1.add_rxq(1)
+        self.rxq2 = self.port1.add_rxq(2)
 
+    def test_port_find_rxq_1(self):
+        self.assertEqual(self.port1.find_rxq_by_id(1), self.rxq1)
 
-	def test_port_find_rxq_1(self):
-		self.assertEqual(self.port1.find_rxq_by_id(1),self.rxq1)
-
-	def test_port_find_rxq_2(self):
-		self.assertEqual(self.port1.find_rxq_by_id(3),None)
-
+    def test_port_find_rxq_2(self):
+        self.assertEqual(self.port1.find_rxq_by_id(3), None)
