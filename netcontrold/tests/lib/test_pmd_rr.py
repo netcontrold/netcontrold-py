58e978defa1a567b43c5514b406df5aa54f00675
Gowrishankar Muthukrishnan <gmuthukr@redhat.com>
2020-03-30 09:24:26 +0530

    Updated rr dryrun tests for better comments and validations.
    Signed-off-by: Gowrishankar Muthukrishnan <gmuthukr@redhat.com>

--- netcontrold/tests/lib/test_pmd_rr.py
+++ netcontrold/tests/lib/test_pmd_rr.py
@@ -106,8 +106,11 @@ class TestRebalDryrunRR_OnePmd(TestCase):
         for i in range(0, config.ncd_samples_max):
             fx_rxq.cpu_cyc[i] = (1000 + (100 * i))
 
+        # test dryrun
         n_reb_rxq = dataif.rebalance_dryrun_rr(self.pmd_map)
 
+        # validate results
+        # 1. no rxqs be rebalanced.
         self.assertEqual(n_reb_rxq, 0)
 
         # del port object from pmd.
@@ -137,18 +140,21 @@ class TestRebalDryrunRR_OnePmd(TestCase):
             for i in range(0, config.ncd_samples_max):
                 fx_rxq.cpu_cyc[i] = (1000 + (100 * i))
 
+        # test dryrun
         n_reb_rxq = dataif.rebalance_dryrun_rr(self.pmd_map)
 
+        # validate results
+        # 1. no rxqs be rebalanced.
         self.assertEqual(n_reb_rxq, 0)
 
+        # del port object from pmd.
         for port_name in ('virtport1', 'virtport2', 'virtport3'):
             pmd.del_port(port_name)
 
+
 # Fixture:
 #   create two pmd thread objects where in, each has one single-queued port.
-
-
-def fx_2pmd_for_1rxq_each(testobj):
+def fx_2pmd_each_1rxq(testobj):
     # retrieve pmd object.
     pmd1 = testobj.pmd_map[testobj.core1_id]
     pmd2 = testobj.pmd_map[testobj.core2_id]
@@ -182,7 +188,7 @@ def fx_2pmd_for_1rxq_each(testobj):
 # Fixture:
 #   Create two pmd thread objects where in, one pmd has two single-queued
 #   ports, while the other is idle (without any port/rxq).
-def fx_1pmd_for_2rxq(testobj):
+def fx_2pmd_one_empty(testobj):
     # retrieve pmd object.
     pmd1 = testobj.pmd_map[testobj.core1_id]
 
@@ -217,7 +223,7 @@ def fx_1pmd_for_2rxq(testobj):
 # Fixture:
 #   Create two pmd thread objects where in, each pmd has two single-queued
 #   ports.
-def fx_2pmd_for_4rxq(testobj):
+def fx_2pmd_each_2rxq(testobj):
     # retrieve pmd object.
     pmd1 = testobj.pmd_map[testobj.core1_id]
     pmd2 = testobj.pmd_map[testobj.core2_id]
@@ -306,24 +312,32 @@ class TestRebalDryrunRR_TwoPmd(TestCase):
     #   With two threads from same numa, each handling only one single-queued
     #   port, check whether rebalance is skipped.
     def test_one_rxq_lnuma(self):
-        # set different numa for pmds
+        # set same numa for pmds
         pmd1 = self.pmd_map[self.core1_id]
         pmd2 = self.pmd_map[self.core2_id]
-
         pmd1.numa_id = 0
         pmd2.numa_id = 0
 
-        fx_2pmd_for_1rxq_each(self)
+        # create rxq
+        fx_2pmd_each_1rxq(self)
 
+        # update pmd load values
         dataif.update_pmd_load(self.pmd_map)
+
+        # copy original pmd objects
         pmd_map = copy.deepcopy(self.pmd_map)
+
+        # test dryrun
         n_reb_rxq = dataif.rebalance_dryrun_rr(self.pmd_map)
 
+        # validate results
+        # 1. all two rxqs be rebalanced.
         self.assertEqual(n_reb_rxq, 0, "no rebalance expected")
+        # 2. each pmd is not updated.
         self.assertEqual(
-            (pmd_map[self.core1_id] == self.pmd_map[self.core1_id]), True)
+            (pmd_map[self.core1_id] == pmd1), True)
         self.assertEqual(
-            (pmd_map[self.core2_id] == self.pmd_map[self.core2_id]), True)
+            (pmd_map[self.core2_id] == pmd2), True)
 
         # del port object from pmd.
         # TODO: create fx_ post deletion routine for clean up
@@ -332,32 +346,61 @@ class TestRebalDryrunRR_TwoPmd(TestCase):
 
     # Test case:
     #   With two threads from same numa, where one pmd thread is handling
-    #   two single-queued ports, while the other is doing nothing,
+    #   two single-queued ports, while the other pmd is empty,
     #   check whether rebalance is performed.
+    #   Scope is to check if only one rxq is moved to empty pmd.
+    #
+    #   order of rxqs based on cpu consumption: rxqp2,rxqp1
+    #   order of pmds for rebalance dryrun: pmd1,pmd2
+    #
+    #   1. rxqp1(pmd1)
+    #      rxqp2(pmd1) -NOREB-> rxqp2(pmd1)
+    #        -  (pmd2)
+    #
+    #   2. rxqp1(pmd1) --+
+    #      rxqp2(pmd1)    \    rxqp2(pmd1)
+    #        -  (pmd2)     +-> rxqp1(reb_pmd2)
+    #
     @mock.patch('netcontrold.lib.util.open')
     def test_two_rxq_lnuma(self, mock_open):
         mock_open.side_effect = [
             mock.mock_open(read_data=_FX_CPU_INFO).return_value
         ]
 
-        # set different numa for pmds
+        # set same numa for pmds
         pmd1 = self.pmd_map[self.core1_id]
         pmd2 = self.pmd_map[self.core2_id]
-
         pmd1.numa_id = 0
         pmd2.numa_id = 0
 
-        fx_1pmd_for_2rxq(self)
+        # create rxq
+        fx_2pmd_one_empty(self)
 
+        # update pmd load values
         dataif.update_pmd_load(self.pmd_map)
+
+        # copy original pmd objects
         pmd_map = copy.deepcopy(self.pmd_map)
+
+        # test dryrun
         n_reb_rxq = dataif.rebalance_dryrun_rr(self.pmd_map)
 
+        # validate results
+        # 1. all two rxqs be rebalanced.
         self.assertEqual(n_reb_rxq, 2, "two rxqs to be rebalanced")
+        # 2. each pmd is updated.
         self.assertEqual(
-            (pmd_map[self.core1_id] != self.pmd_map[self.core1_id]), True)
+            (pmd_map[self.core1_id] != pmd1), True)
         self.assertEqual(
-            (pmd_map[self.core2_id] != self.pmd_map[self.core2_id]), True)
+            (pmd_map[self.core2_id] != pmd2), True)
+        # 3. check rxq map after dryrun.
+        port1 = pmd1.find_port_by_name('virtport1')
+        port2 = pmd1.find_port_by_name('virtport2')
+        # 3.a rxqp2 remains in pmd1
+        self.assertEqual(port2.rxq_rebalanced, {})
+        self.assertEqual(port2.find_rxq_by_id(0).pmd.id, pmd1.id)
+        # 3.a rxqp1 moves into pmd2
+        self.assertEqual(port1.rxq_rebalanced[0], pmd2.id)
 
         # del port object from pmd.
         # TODO: create fx_ post deletion routine for clean up
@@ -395,7 +438,7 @@ class TestRebalDryrunRR_TwoPmd(TestCase):
         pmd2.numa_id = 0
 
         # create rxq
-        fx_2pmd_for_4rxq(self)
+        fx_2pmd_each_2rxq(self)
 
         # update pmd load values
         dataif.update_pmd_load(self.pmd_map)
@@ -426,29 +469,59 @@ class TestRebalDryrunRR_TwoPmd(TestCase):
         pmd2.del_port('virtport2')
 
     # Test case:
-    #   With two threads from different numa, each handling one single-queued
-    #   port, check rebalance is skipped.
+    #   With two threads from pmd of one numa, rebalance should be
+    #   skipped even though a pmd from other numa is available and
+    #   empty.
+    #   Scope is to check if nonlocal pmd continues be empty after dryrun.
+    #
+    #   order of rxqs based on cpu consumption: rxqp2,rxqp1
+    #
+    #   1. rxqp1(pmd1N0)
+    #      rxqp2(pmd1N0) -NOREB-> rxqp2(pmd1N0)
+    #        -  (pmd2N1)
+    #
+    #   2. rxqp1(pmd1N0) -NOREB-> rxqp1(pmd1N0)
+    #      rxqp2(pmd1N0)          rxqp2(pmd1N0)
+    #        -  (pmd2N1)
+    #
     def test_one_rxq_rnuma(self):
         # set different numa for pmds
         pmd1 = self.pmd_map[self.core1_id]
         pmd2 = self.pmd_map[self.core2_id]
-
         pmd1.numa_id = 0
         pmd2.numa_id = 1
 
-        fx_2pmd_for_1rxq_each(self)
+        # create rxq
+        fx_2pmd_one_empty(self)
 
+        # update pmd load values
         dataif.update_pmd_load(self.pmd_map)
+
+        # copy original pmd objects
         pmd_map = copy.deepcopy(self.pmd_map)
+
+        # test dryrun
         n_reb_rxq = dataif.rebalance_dryrun_rr(self.pmd_map)
 
-        self.assertEqual(n_reb_rxq, 0, "no rebalance expected")
+        # validate results
+        # 1. no rxq be rebalanced.
+        self.assertEqual(n_reb_rxq, 2, "rebalance only in local numa expected")
+        # 2. no pmd is updated.
         self.assertEqual(
-            (pmd_map[self.core1_id] == self.pmd_map[self.core1_id]), True)
+            (pmd_map[self.core1_id] == pmd1), True)
         self.assertEqual(
-            (pmd_map[self.core2_id] == self.pmd_map[self.core2_id]), True)
+            (pmd_map[self.core2_id] == pmd2), True)
+        # 3. check rxq map after dryrun.
+        port1 = pmd1.find_port_by_name('virtport1')
+        port2 = pmd1.find_port_by_name('virtport2')
+        # 3.a rxqp2 remains in pmd1
+        self.assertEqual(port2.rxq_rebalanced, {})
+        self.assertEqual(port2.find_rxq_by_id(0).pmd.id, pmd1.id)
+        # 3.a rxqp1 moves into pmd2
+        self.assertEqual(port1.rxq_rebalanced, {})
+        self.assertEqual(port1.find_rxq_by_id(0).pmd.id, pmd1.id)
 
         # del port object from pmd.
         # TODO: create fx_ post deletion routine for clean up
         pmd1.del_port('virtport1')
-        pmd2.del_port('virtport2')
+        pmd1.del_port('virtport2')
